80a5fb73da064f5d01d8c036a3c828fe9c41ad79
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-04-28 12:15:35 +0700

    updated docs

---
+++ README.md
@@ -0,0 +1,33 @@
+## /all-student-name test report
+
+Table
+![img.png](img.png)
+
+Graph
+![img_1.png](img_1.png)
+
+Result Tree
+![img_3.png](img_3.png)
+
+Summary
+![img_2.png](img_2.png)
+
+### log result
+![img_8.png](img_8.png)
+
+## /highest-gpa test report
+
+Table
+![img_6.png](img_6.png)
+
+Graph
+![img_7.png](img_7.png)
+
+Result Tree
+![img_4.png](img_4.png)
+
+Summary
+![img_5.png](img_5.png
+
+### log result
+![img_9.png](img_9.png)
---
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -0,0 +1,20 @@
+package com.advpro.profiling.tutorial.repository;
+
+import com.advpro.profiling.tutorial.model.Student;
+import com.advpro.profiling.tutorial.model.StudentCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author muhammad.khadafi
+ */
+@Repository
+public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
+    List<StudentCourse> findByStudentId(Long studentId);
+
+    @Query("SELECT sc FROM StudentCourse sc JOIN FETCH sc.student JOIN FETCH sc.course")
+    List<StudentCourse> findAllWithStudentAndCourse();
+}
